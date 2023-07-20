6db558a96733e2487c0f744319314230d42255c6
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2023-07-20 13:25:37 -0600

    fix(save.js): set custom headers to match browsers
    Without this change, some websites were blocking my image downloads. I
    updated the headers to match exactly what is sent by my browser
    (Firefox) when requesting images for the webpage.

--- save.js
+++ save.js
@@ -1,40 +1,71 @@
-const fs = require('fs');
-const client = require('https');
+const fs = require("fs");
+const client = require("https");
+const ProgressBar = require("progress");
 
-function downloadImage(url, filepath) {
+function downloadImage(uri, filepath) {
+  const url = new URL(uri);
   return new Promise((resolve, reject) => {
-    client.get(url, (res) => {
-      if (res.statusCode === 200) {
-        res
-          .pipe(fs.createWriteStream(filepath))
-          .on('error', reject)
-          .once('close', () => resolve(filepath));
-      } else {
-        // Consume response data to free up memory
-        res.resume();
-        reject(
-          new Error(`Request Failed With a Status Code: ${res.statusCode}`)
-        );
+    client.get(
+      {
+        hostname: url.hostname,
+        path: `${url.pathname}${url.search}`,
+        protocol: url.protocol,
+        headers: {
+          "Accept": "image/avif,image/webp,*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Host": "www.isabelmarant.com",
+          "Referer": "https://www.isabelmarant.com/us/lookbooks/isabel-marant/isabel-marant-fall-winter-2023/",
+          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/115.0",
+        },
+      },
+      (res) => {
+        if (res.statusCode === 200) {
+          res
+            .pipe(fs.createWriteStream(filepath))
+            .on("error", reject)
+            .once("close", () => resolve(filepath));
+        } else {
+          // Consume response data to free up memory
+          res.resume();
+          reject(
+            new Error(`Request Failed With a Status Code: ${res.statusCode}`)
+          );
+        }
       }
-    });
+    );
   });
 }
 
 function getLookNumber(index) {
-  return (index + 1).toLocaleString(undefined, { minimumIntegerDigits: 2 })
+  return (index + 1).toLocaleString(undefined, { minimumIntegerDigits: 2 });
 }
 
 function getImageURL(index) {
   const url =
-    `https://assets.hermes.com/is/image/hermesedito/` +
-    `3x4_RUNWAY_HERMES_DEFILE_PAPHPE23_FILIPPOFIOR_` +
-    `${getLookNumber(index)}?fit=wrap%2C0&wid=1440`
+    `https://www.isabelmarant.com/app/uploads/2023/02/Isabel-Marant-FW23-` +
+    `${getLookNumber(index)}-1.jpg`;
   return url;
 }
 
-async function save(count = 53, show = 'hermes/SS23', ext = 'webp') {
-  const urls = Array(count).fill(null).map((_, index) => getImageURL(index));
-  return Promise.all(urls.map((url, index) => downloadImage(url, `shows/${show}/${getLookNumber(index)}.${ext}`)));
+async function save(count = 53, show = "hermes/SS23", ext = "webp") {
+  const urls = Array(count)
+    .fill(null)
+    .map((_, index) => getImageURL(index));
+  const bar = new ProgressBar(
+    "saving [:bar] :rate/pps :current/:total :percent :etas",
+    {
+      complete: "=",
+      incomplete: " ",
+      width: 20,
+      total: urls.length,
+    }
+  );
+  for await (const url of urls) {
+    const index = urls.indexOf(url);
+    await downloadImage(url, `shows/${show}/${getLookNumber(index)}.${ext}`);
+    bar.tick();
+  }
 }
 
-void save();
+void save(59, "isabel-marant/FW23", "jpg");
